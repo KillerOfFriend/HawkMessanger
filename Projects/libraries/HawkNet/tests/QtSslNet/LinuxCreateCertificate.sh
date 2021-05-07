2434cdbdc08f8d408ab20736d9624ee512fd8bad
KillerOfFriend <killeroffriend@yandex.ru>
2021-05-07 13:07:12 +0300

    07.05.2021 Отвязка от expect script для генерации сертификатов на linux

--- Projects/libraries/HawkNet/tests/QtSslNet/LinuxCreateCertificate.sh
+++ Projects/libraries/HawkNet/tests/QtSslNet/LinuxCreateCertificate.sh
@@ -1,44 +1,78 @@
-#!/usr/bin/expect -f
+#!/bin/bash
 
-set timeout -1
+#--------------------------------------------------------------------------------
 
-set TARGET_PATH [lindex $argv 0]
+# Получаем путь к место назначения из первого параметра
+TARGET_PATH=$1
+echo "TARGET_PATH = $TARGET_PATH"
 
-#Если путь к месту назначению пуст то файлы создадим в текущей директории
-if {$TARGET_PATH eq ""} {
-    set CERT_FILE_NAME certificate.crt
-    set PRKEY_FILE_NAME privateKey.key
-} else {
-    set CERT_FILE_NAME $TARGET_PATH/certificate.crt
-    set PRKEY_FILE_NAME $TARGET_PATH/privateKey.key
-}
+if [ -z "$TARGET_PATH" ]
+then
+    # Если путь к месту назначению не задан то файлы создадим в текущей директории
+    CERT_FILE_NAME=certificate.crt
+    PRKEY_FILE_NAME=privateKey.key
+else
+    # Если путь к месту назначению задан создадим в по месту назначения
+    CERT_FILE_NAME=$TARGET_PATH/certificate.crt
+    PRKEY_FILE_NAME=$TARGET_PATH/privateKey.key
+    # Если директория места назначения не существует то создадим её
+    if [[ ! -e $TARGET_PATH ]]; then
+        mkdir -p $TARGET_PATH
+    fi
+fi
 
-#---------------------------
+# Удаляем старые файлы, если они существуют
+rm $CERT_FILE_NAME
+rm $PRKEY_FILE_NAME
 
-spawn rm $CERT_FILE_NAME
-spawn rm $PRKEY_FILE_NAME
+#--------------------------------------------------------------------------------
 
-spawn openssl req -x509 -sha256 -nodes -days 365 -newkey rsa:2048 -keyout $PRKEY_FILE_NAME -out $CERT_FILE_NAME
+# Задаём конфигурации формирования сертификата
+CONFIGNAME=LinuxSslConf.cfg
 
-expect "Country Name (2 letter code)"
-send -- "RU\r"
+HOST_NAME="example"
+DOT=ru
+COUNTRY=RU
+STATE=LOBL
+CITY=SPB
+#ORGANIZATION=IT
+#ORGANIZATION_UNIT=IT Department
+#EMAIL=webmaster@$HOST_NAME.$DOT
 
-expect "State or Province Name (full name)"
-send -- "\r"
+#--------------------------------------------------------------------------------
 
-expect "Locality Name (eg, city)"
-send -- "SPB\r"
+# Формируем файл конфигураций
+{
+echo [req]
+echo default_bits = 2048
+echo prompt = no
+echo default_md = sha256
+echo x509_extensions = v3_req
+echo distinguished_name = dn
+echo
+echo [dn]
+echo C = $COUNTRY
+echo ST = $STATE
+echo L = $CITY
+# echo O = $ORGANIZATION
+# echo OU = $ORGANIZATION_UNIT
+# echo emailAddress = $EMAIL
+echo CN = $HOST_NAME.$DOT
+echo
+echo [v3_req]
+echo subjectAltName = @alt_names
+echo
+echo [alt_names]
+echo DNS.1 = *.$HOST_NAME.$DOT
+echo DNS.2 = $HOST_NAME.$DOT
+} >> $CONFIGNAME
 
-expect "Organization Name (eg, company)"
-send -- "\r"
+#--------------------------------------------------------------------------------
 
-expect "Organizational Unit Name (eg, section)"
-send -- "\r"
+# Вызываем OpenSSL для формирования сертификата и приватного ключа на основе файла конфигураций
+openssl req -new -x509 -newkey rsa:2048 -sha256 -nodes -keyout $PRKEY_FILE_NAME -days 356 -out $CERT_FILE_NAME -config $CONFIGNAME
+# Удаляем файл клнфигураций
+rm $CONFIGNAME
 
-expect "Common Name (e.g. server FQDN or YOUR name)"
-send -- "BE_GET\r"
+#--------------------------------------------------------------------------------
 
-expect "Email Address"
-send -- "\r"
-
-expect eof
--- Projects/libraries/HawkNet/tests/QtSslNet/WindowsCreateCertificate.bat
+++ Projects/libraries/HawkNet/tests/QtSslNet/WindowsCreateCertificate.bat
@@ -1,16 +1,29 @@
+::--------------------------------------------------------------------------------
+
+:: Получаем путь к место назначения из первого параметра
 @SET TARGET_PATH=%1
 
 @IF "%TARGET_PATH%" == "" (
+    :: Если путь к месту назначению не задан то файлы создадим в текущей директории
 	@SET CERT_FILE_NAME=certificate.crt
 	@SET PRKEY_FILE_NAME=privateKey.key
 ) ELSE (
+    :: Если путь к месту назначению задан создадим в по месту назначения
     @SET CERT_FILE_NAME=%TARGET_PATH%/certificate.crt
     @SET PRKEY_FILE_NAME=%$TARGET_PATH%/privateKey.key
+    :: Если директория места назначения не существует то создадим её
+    IF NOT EXIST "%TARGET_PATH%" (
+        mkdir -p "%TARGET_PATH%"
+    )
 )
 
+:: Удаляем старые файлы, если они существуют
 del %PRKEY_FILE_NAME%
 del %CERT_FILE_NAME%
 
+::--------------------------------------------------------------------------------
+
+:: Задаём конфигурации формирования сертификата
 @SET CONFIGNAME=WinSslConf.cfg
 
 @SET HOSTNAME=example
@@ -18,10 +31,13 @@ del %CERT_FILE_NAME%
 @SET COUNTRY=RU
 @SET STATE=LOBL
 @SET CITY=SPB
-@REM SET ORGANIZATION=IT
-@REM SET ORGANIZATION_UNIT=IT Department
-@REM SET EMAIL=webmaster@%HOSTNAME%.%DOT%
+:: SET ORGANIZATION=IT
+:: SET ORGANIZATION_UNIT=IT Department
+:: SET EMAIL=webmaster@%HOSTNAME%.%DOT%
 
+::--------------------------------------------------------------------------------
+
+:: Формируем файл конфигураций
 @(
 @echo [req]
 @echo default_bits = 2048
@@ -34,9 +50,9 @@ del %CERT_FILE_NAME%
 @echo C = %COUNTRY%
 @echo ST = %STATE%
 @echo L = %CITY%
-@REM echo O = %ORGANIZATION%
-@REM echo OU = %ORGANIZATION_UNIT%
-@REM echo emailAddress = %EMAIL%
+:: echo O = %ORGANIZATION%
+:: echo OU = %ORGANIZATION_UNIT%
+:: echo emailAddress = %EMAIL%
 @echo CN = %HOSTNAME%.%DOT%
 @echo:
 @echo [v3_req]
@@ -47,6 +63,11 @@ del %CERT_FILE_NAME%
 @echo DNS.2 = %HOSTNAME%.%DOT%
 )>%CONFIGNAME%
 
-openssl req -new -x509 -newkey rsa:2048 -sha256 -nodes -keyout %PRKEY_FILE_NAME% -days 356 -out %CERT_FILE_NAME% -config %CONFIGNAME%
+::--------------------------------------------------------------------------------
 
+:: Вызываем OpenSSL для формирования сертификата и приватного ключа на основе файла конфигураций
+openssl req -new -x509 -newkey rsa:2048 -sha256 -nodes -keyout %PRKEY_FILE_NAME% -days 356 -out %CERT_FILE_NAME% -config %CONFIGNAME%
+:: Удаляем файл клнфигураций
 del %CONFIGNAME%
+
+::--------------------------------------------------------------------------------
