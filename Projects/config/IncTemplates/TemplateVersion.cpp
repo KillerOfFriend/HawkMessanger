8d08fc4ca8fc97aad2c619340ec7482a4b77b386
KillerOfFriend <killeroffriend@yandex.ru>
2021-05-16 20:25:52 +0300

    16.05.2021 Фикс автогенерации файлов версий

--- Projects/config/IncTemplates/TemplateVersion.cpp
+++ Projects/config/IncTemplates/TemplateVersion.cpp
@@ -1,16 +1,16 @@
 #include "@VERSION_FILE_NAME@.h"
 
-using namespace @PROJECT_NAME@;
-
 //-----------------------------------------------------------------------------
-std::string getVersion()    { return "@PROJECT_VERSION@"; }
+std::string @PROJECT_NAME@::getVersion()    { return "@PROJECT_VERSION@"; }
 //-----------------------------------------------------------------------------
-std::uint8_t getVerMajor()  { return @PROJECT_VERSION_MAJOR@; }
+std::uint8_t @PROJECT_NAME@::getVerMajor()  { return @PROJECT_VERSION_MAJOR@; }
 //-----------------------------------------------------------------------------
-std::uint8_t getVerMinor()  { return @PROJECT_VERSION_MINOR@; }
+std::uint8_t @PROJECT_NAME@::getVerMinor()  { return @PROJECT_VERSION_MINOR@; }
 //-----------------------------------------------------------------------------
-std::uint8_t getVerPatch()  { return @PROJECT_VERSION_PATCH@; }
+std::uint8_t @PROJECT_NAME@::getVerPatch()  { return @PROJECT_VERSION_PATCH@; }
 //-----------------------------------------------------------------------------
-std::uint8_t getVerTweak()  { return @PROJECT_VERSION_TWEAK@; }
+std::uint8_t @PROJECT_NAME@::getVerTweak()  { return @PROJECT_VERSION_TWEAK@; }
+//-----------------------------------------------------------------------------
+std::string @PROJECT_NAME@::getModuleName() { return "@PROJECT_NAME@"; }
 //-----------------------------------------------------------------------------
 
